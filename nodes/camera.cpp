571d6eaf575adf7d6121b54275e93963c3262531
Christopher Berner <christopherberner@gmail.com>
2015-04-24 20:13:43 -0700

    Rebase from 'debian/indigo/rospilot'

---
+++ nodes/camera.cpp
@@ -0,0 +1,359 @@
+/*********************************************************************
+ *
+ * Copyright 2012 the original author or authors.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *********************************************************************/
+#include<stdio.h>
+#include<time.h>
+#include<fstream>
+#include<chrono>
+#include<boost/filesystem.hpp>
+#include<wordexp.h>
+#include<dirent.h>
+#include<sys/ioctl.h>
+#include<fcntl.h>
+
+#include<ros/ros.h>
+#include<rospilot/CaptureImage.h>
+#include<std_srvs/Empty.h>
+#include<sensor_msgs/CompressedImage.h>
+
+#include<ptp.h>
+#include<usb_camera.h>
+#include<video_recorder.h>
+#include<transcoders.h>
+
+extern "C" {
+#include <linux/videodev2.h>
+}
+
+using namespace std::chrono;
+
+class CameraNode
+{
+private:
+    ros::NodeHandle node;
+    // NOTE: We don't need to guard this with a mutex, because callbacks
+    // are called in spinOnce() in the main thread
+    BaseCamera *camera = nullptr;
+    JpegDecoder *jpegDecoder = nullptr;
+    H264Encoder *h264Encoder = nullptr;
+    SoftwareVideoRecorder *videoRecorder = nullptr;
+    ros::Publisher imagePub;
+    ros::ServiceServer captureServiceServer;
+    ros::ServiceServer startRecordServiceServer;
+    ros::ServiceServer stopRecordServiceServer;
+
+    std::string videoDevice;
+    std::string codec; // "mjpeg" or "h264"
+    CodecID codecId;
+    std::string mediaPath;
+    int width;
+    int height;
+    int framerate;
+
+private:
+    bool sendPreview()
+    {
+        static time_point<high_resolution_clock> sixtyFramesAgo = high_resolution_clock::now();
+        static int frameRateCounter = 0;
+
+        sensor_msgs::CompressedImage image;
+        if(camera != nullptr && camera->getLiveImage(&image)) {
+            bool keyFrame = false;
+            bool transcodedSuccessfully = false;
+            if (codec == "mjpeg") {
+                keyFrame = true;
+                transcodedSuccessfully = true;
+            }
+            imagePub.publish(image);
+            if (codec == "h264" && image.format == "jpeg") {
+                jpegDecoder->decodeInPlace(&image);
+                transcodedSuccessfully = h264Encoder->encodeInPlace(&image, 
+                        &keyFrame);
+            }
+            if (videoRecorder != nullptr && transcodedSuccessfully) {
+                videoRecorder->writeFrame(&image, keyFrame);
+            }
+
+            frameRateCounter++;
+            if (frameRateCounter >= 60) {
+                time_point<high_resolution_clock> currentTime = high_resolution_clock::now();
+                duration<double> duration = (currentTime - sixtyFramesAgo);
+                //ROS_INFO("Camera frame rate %f", frameRateCounter / duration.count());
+                frameRateCounter = 0;
+                sixtyFramesAgo = currentTime;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    void initCameraAndEncoders()
+    {
+        if (camera != nullptr) {
+            delete camera;
+        }
+        camera = createCamera();
+        PixelFormat pixelFormat;
+        if (codec == "h264") {
+            pixelFormat = PIX_FMT_YUV420P;
+        }
+        else if (codec == "mjpeg") {
+            // TODO: Do we need to detect this dynamically?
+            // Different cameras might be 4:2:0, 4:2:2, or 4:4:4
+            pixelFormat = PIX_FMT_YUVJ422P;
+        }
+
+        std::string videoDevice;
+        node.param("video_device", videoDevice, std::string("/dev/video0"));
+
+        if (jpegDecoder != nullptr) {
+            delete jpegDecoder;
+        }
+        jpegDecoder = new JpegDecoder(width, height, pixelFormat);
+        if (h264Encoder != nullptr) {
+            delete h264Encoder;
+        }
+        h264Encoder = createEncoder();
+        if (videoRecorder != nullptr) {
+            delete videoRecorder;
+        }
+        ROS_INFO("Recording in %s", codec.c_str());
+        videoRecorder = new SoftwareVideoRecorder(width, height, pixelFormat, codecId);
+    }
+
+    BaseCamera *createCamera()
+    {
+        std::string cameraType;
+        node.param("camera_type", cameraType, std::string("usb"));
+
+        node.param("video_device", videoDevice, std::string("/dev/video0"));
+        node.param("image_width", width, 1920);
+        node.param("image_height", height, 1080);
+        node.param("framerate", framerate, 30);
+        node.param("codec", codec, std::string("mjpeg"));
+        if (codec == "h264") {
+            codecId = CODEC_ID_H264;
+        }
+        else if (codec == "mjpeg") {
+            codecId = CODEC_ID_MJPEG;
+        }
+        else {
+            ROS_FATAL("Unknown codec: %s", codec.c_str());
+        }
+        node.param("media_path", mediaPath, std::string("~/.rospilot/media"));
+        wordexp_t p;
+        wordexp(mediaPath.c_str(), &p, 0);
+        if (p.we_wordc != 1) {
+            ROS_ERROR("Got too many words when expanding media path: %s",
+                    mediaPath.c_str());
+        }
+        else {
+            mediaPath = p.we_wordv[0];
+        }
+        wordfree(&p);
+
+
+        if (cameraType == "ptp") {
+            return new PtpCamera();
+        }
+        else if (cameraType == "usb") {
+            ROS_INFO("Requesting camera res %dx%d", width, height);
+            UsbCamera *camera = new UsbCamera(videoDevice, width, height, framerate);
+            // Read the width and height, since the camera may have altered it to
+            // something it supports
+            width = camera->getWidth();
+            height = camera->getHeight();
+            ROS_INFO("Camera selected res %dx%d", width, height);
+            return camera;
+        }
+        else {
+            ROS_FATAL("Unsupported camera type: %s", cameraType.c_str());
+            return nullptr;
+        }
+    }
+
+public:
+    CameraNode() : node("~") 
+    {
+        imagePub = node.advertise<sensor_msgs::CompressedImage>(
+                "image_raw/compressed", 1);
+        captureServiceServer = node.advertiseService(
+                "capture_image", 
+                &CameraNode::captureImageCallback,
+                this);
+        startRecordServiceServer = node.advertiseService(
+                "start_record", 
+                &CameraNode::startRecordHandler,
+                this);
+        stopRecordServiceServer = node.advertiseService(
+                "stop_record", 
+                &CameraNode::stopRecordHandler,
+                this);
+        initCameraAndEncoders();
+    }
+
+    bool mfcDeviceIsPresent()
+    {
+        // Look for the MFC
+        DIR *dir = opendir("/dev");
+        dirent *dirEntry = nullptr;
+        while ((dirEntry = readdir(dir)) != nullptr) {
+            int fd;
+            v4l2_capability videoCap;
+            std::string path = std::string("/dev/") + dirEntry->d_name;
+            if (path.substr(0, std::string("/dev/video").size()) != "/dev/video" ||
+                    path == videoDevice) {
+                continue;
+            }
+            ROS_INFO("Querying %s", path.c_str());
+            if((fd = open(path.c_str(), O_RDONLY)) == -1){
+                ROS_WARN("Can't open %s", path.c_str());
+                continue;
+            }
+
+            if(ioctl(fd, VIDIOC_QUERYCAP, &videoCap) == -1) {
+                ROS_WARN("Can't read from %s", path.c_str());
+            }
+            else {
+                if (std::string((char *) videoCap.driver) == "s5p-mfc") {
+                    v4l2_ext_controls ctrls;
+                    v4l2_ext_control ctrl;
+
+                    ctrl.id = V4L2_CID_MPEG_VIDEO_H264_I_PERIOD;
+                    // These aren't used for integer controls
+                    ctrl.size = 0;
+                    memzero(ctrl.reserved2);
+
+                    ctrls.ctrl_class = V4L2_CTRL_CLASS_MPEG;
+                    ctrls.count = 1;
+                    ctrls.controls = &ctrl;
+                    
+                    if(ioctl(fd, VIDIOC_G_EXT_CTRLS, &ctrls) == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        closedir(dir);
+        return false;
+    }
+
+    H264Encoder *createEncoder()
+    {
+        if (mfcDeviceIsPresent()) {
+            ROS_INFO("Using hardware encoder");
+            return new ExynosMultiFormatCodecH264Encoder(width, height);
+        }
+        else {
+            return new SoftwareH264Encoder(width, height);
+        }
+    }
+
+    ~CameraNode()
+    {
+        if (camera != nullptr) {
+            delete camera;
+        }
+        delete jpegDecoder;
+        delete h264Encoder;
+        delete videoRecorder;
+    }
+
+    bool spin()
+    {
+        ROS_INFO("camera node is running.");
+                
+        while (node.ok())
+        {
+            // Process any pending service callbacks
+            ros::spinOnce();
+            std::string newVideoDevice;
+            node.getParam("video_device", newVideoDevice);
+            std::string newCodec;
+            node.param("codec", newCodec, std::string("mjpeg"));
+            if (newVideoDevice != videoDevice || newCodec != codec) {
+                initCameraAndEncoders();
+            }
+            if(!sendPreview()) {
+                // Sleep and hope the camera recovers
+                usleep(1000*1000);
+            }
+            // Run at 1kHz
+            usleep(1000);
+        }
+        return true;
+    }
+    
+    bool startRecordHandler(std_srvs::Empty::Request& request, 
+            std_srvs::Empty::Response &response)
+    {
+        time_t t = time(nullptr);
+        struct tm *tmp = localtime(&t);
+        char str[100];
+        strftime(str, sizeof(str), "%Y-%m-%d_%H%M%S.mp4", tmp);
+        std::string path = mediaPath + "/" + str;
+        // Reset the encoder
+        if (h264Encoder != nullptr) {
+            delete h264Encoder;
+        }
+        h264Encoder = createEncoder();
+        return videoRecorder->start(path.c_str());
+    }
+    
+    bool stopRecordHandler(std_srvs::Empty::Request& request, 
+            std_srvs::Empty::Response &response)
+    {
+        return videoRecorder->stop();
+    }
+
+    bool captureImageCallback(std_srvs::Empty::Request& request, 
+            std_srvs::Empty::Response &response)
+    {
+        if (camera != nullptr) {
+            time_t t = time(nullptr);
+            struct tm *tmp = localtime(&t);
+            char str[100];
+            strftime(str, sizeof(str), "%Y-%m-%d_%H%M%S", tmp);
+            std::string basePath = mediaPath + "/" + str;
+            std::string path = basePath + ".jpg";
+            for (int i = 1; boost::filesystem::exists(path); i++) {
+                std::stringstream ss;
+                ss << basePath << "_" << i << ".jpg";
+                path = ss.str();
+            }
+            sensor_msgs::CompressedImage image;
+            if (!camera->captureImage(&image)) {
+                return false;
+            }
+            std::fstream fout(path, std::fstream::out);
+            fout.write((char*) image.data.data(), image.data.size());
+            fout.close();
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "camera");
+    CameraNode a;
+    a.spin();
+    return 0;
+}
--- nodes/web_ui.py
+++ nodes/web_ui.py
@@ -23,43 +23,59 @@ roslib.load_manifest('rospilot')
 import rospy
 import json
 import cherrypy
-import threading
 import os
+import re
 import glob
 import std_srvs.srv
 import rospilot.srv
-import sensor_msgs.msg
 import urllib2
-import time
-import pkg_resources
+import cv2
 from optparse import OptionParser
+from catkin.find_in_workspaces import find_in_workspaces
 
-STATIC_PATH = pkg_resources.resource_filename('rospilot.assets', '')
+STATIC_PATH = find_in_workspaces(['share'], 'rospilot',
+                                 'share/web_assets/', first_match_only=True)[0]
 
 PORT_NUMBER = 8085
 
 
 class API(object):
-    def __init__(self, node):
-        self.node = node
+    def __init__(self, media_path):
+        self.media_path = media_path
 
     @cherrypy.expose
-    def media(self):
-        paths = []
-        with self.node.lock:
-            paths = os.listdir(self.node.media_path)
-        paths = ['/media/' + path for path in paths]
-        objs = []
-        for path in reversed(sorted(paths)):
-            if path.endswith('jpg'):
-                objs.append({"type": "image", "url": path})
-            else:
-                objs.append({"type": "video", "url": path})
-        return json.dumps({'objs': objs})
+    def media(self, id=None):
+        if cherrypy.request.method == "DELETE":
+            if not re.match(r"([a-z]+\.)?[a-zA-Z0-9_-]+\.\w{2,5}", id):
+                rospy.logwarn("Ignoring request to delete %s", id)
+                return
+            os.remove(self.media_path + "/" + id)
+        elif cherrypy.request.method == "GET":
+            paths = os.listdir(self.media_path)
+            objs = []
+            for path in reversed(sorted(paths)):
+                if path.endswith('jpg'):
+                    objs.append({"type": "image", "url": "/media/" + path,
+                                "id": path})
+                else:
+                    thumbnail = "/api/thumbnail?filename=" + path
+                    objs.append({"type": "video", "url": "/media/" + path,
+                                 "thumbnail": thumbnail,
+                                 "id": path})
+            return json.dumps(objs)
+
+    @cherrypy.expose
+    def thumbnail(self, filename):
+        cap = cv2.VideoCapture(self.media_path + "/" + filename)
+        ret, frame = cap.read()
+        ret, jpg = cv2.imencode(".jpg", frame)
+        cap.release()
+        cherrypy.response.headers['Content-Type'] = 'image/jpeg'
+        return jpg.tostring()
 
     @cherrypy.expose
     def camera(self, action):
-        url = 'http://localhost:8080/snapshot?topic=/camera/image_raw/compressed'
+        url = 'http://localhost:8080/snapshot?topic=/rospilot/camera/image_raw/compressed'
         resp = urllib2.urlopen(url)
         cherrypy.response.headers['Content-Type'] = resp.info()['Content-Type']
         return resp.read()
@@ -68,23 +84,17 @@ class API(object):
 class Index(object):
     @cherrypy.expose
     def index(self):
-        return open(os.path.join(STATIC_PATH, "index.html"))
+        return open(os.path.join(STATIC_PATH, "index.html")).read()
 
 
 class WebUiNode(object):
     def __init__(self, media_path):
-        rospy.Subscriber('camera/image_raw/compressed',
-                         sensor_msgs.msg.CompressedImage, self.handle_image)
-        rospy.Service('take_picture',
-                      std_srvs.srv.Empty,
-                      self.take_picture)
         rospy.Service('glob',
                       rospilot.srv.Glob,
                       self.handle_glob)
-        self.lock = threading.Lock()
-        self.last_image = None
-        self.ptp_capture_image = rospy.ServiceProxy('camera/capture_image',
-                                                    rospilot.srv.CaptureImage)
+        rospy.Service('shutdown',
+                      std_srvs.srv.Empty,
+                      self.handle_shutdown)
         self.media_path = os.path.expanduser(media_path)
         if not os.path.exists(self.media_path):
             os.makedirs(self.media_path)
@@ -102,31 +112,15 @@ class WebUiNode(object):
                        }
         }
         index = Index()
-        index.api = API(self)
+        index.api = API(self.media_path)
         cherrypy.tree.mount(index, config=conf)
         cherrypy.log.screen = False
-
-    def handle_image(self, data):
-        with self.lock:
-            self.last_image = data
 
     def handle_glob(self, request):
         return rospilot.srv.GlobResponse(glob.glob(request.pattern))
 
-    def take_picture(self, request):
-        if self.ptp_capture_image is not None:
-            image = self.ptp_capture_image().image
-        else:
-            image = self.last_image
-
-        with self.lock:
-            next_id = int(round(time.time() * 1000))
-
-            filename = "{0:05}.jpg".format(next_id)
-            path = "{0}/{1}".format(self.media_path, filename)
-
-            with open(path, 'w') as f:
-                f.write(image.data)
+    def handle_shutdown(self, request):
+        os.system('shutdown now -P')
         return std_srvs.srv.EmptyResponse()
 
     def run(self):
